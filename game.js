a27f5ffd936236c972f3ffba0962baea7e5a54a9
doug-bill <65959310+doug-bill@users.noreply.github.com>
2025-10-08 23:43:29 -0300

    Initial logic done.
    Getting the Human and CPU choices.
    
    TODO
    - Setup the human choice to be flexible, accepting
    both the number or the word.
    
    - Compare both Human and CPU choices and update the score.
    - make a new loop for a best of 3 or 5.

--- game.js
+++ game.js
@@ -1,13 +1,43 @@
-console.log("Hello");
-
 var humanScore = 0;
-var computerScore =0;
-var cpu; 
+var computerScore = 0;
 
-function getComputerChoice(){
-    
-    cpu = Math.random(3);
-    return cpu;
+function getComputerChoice(max) {
+  
+   var c = Math.floor(Math.random() * max);
+
+   if (c == 0) {
+      return ("Rock");
+   }else if (c == 1) {
+      return ("Paper");
+   }else {
+      return ("Scissors");
+   }
 }
 
-console.log("cpu choice is:" + cpu);
+function getHumanChoice(){
+   
+    let humanChoice = prompt("Type your choice for the Game:\n 1.Rock\n 2.Paper \n 3.Scissors");
+
+
+    return humanChoice;
+}
+
+
+function capitalize(string){
+
+const lower = string.toLowerCase();
+const firstLetter = lower.slice(0,1);
+const firstUpper = firstLetter.toUpperCase();
+const result = firstUpper + lower.slice(1);
+return(result);
+}
+
+//console.log(capitalize(getHumanChoice()));
+   
+
+console.log("Computer Choose: " + getComputerChoice(3));
+console.log("Human Chose: " + getHumanChoice());
+
+
+console.log("Human: "+ humanScore);
+console.log("CPU: "+ computerScore);
