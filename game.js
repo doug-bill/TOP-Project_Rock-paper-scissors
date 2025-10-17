a8a20008b345fb666991d372cddeea40369e4afd
doug-bill <65959310+doug-bill@users.noreply.github.com>
2025-10-16 22:40:46 -0300

    Created the Round function but it has bugs
    Bug to fix:
    Fix the bug when chossing Rock vs Paper-
      The score does not show and jumps to the end of the code
      just  showing the options chosen.
    
      Proposal: Only work on a single "VS" then move to the others.

--- game.js
+++ game.js
@@ -17,27 +17,66 @@ function getComputerChoice(max) {
 function getHumanChoice(){
    
     let humanChoice = prompt("Type your choice for the Game:\n 1.Rock\n 2.Paper \n 3.Scissors");
+     
+    const lower = humanChoice.toLowerCase();
+    const firstLetter = lower.slice(0,1);
+    const firstUpper = firstLetter.toUpperCase();
+    const result = firstUpper + lower.slice(1);
+    return (result);
+}
 
+function playRound (){
 
-    return humanChoice;
+   if (humanSelection == "Rock" && computerSelection == "Rock"){
+      console.log("DRAW");
+     
+   } else if (humanSelection == "Rock" && computerSelection == "Paper"){
+      console.log("Computer won this round");
+      computerScore += 1;
+   } else if (humanSelection == "Rock" && computerSelection == "Scissors"){
+      console.log("You've won this round");
+      humanScore += 1;
+   } else if (humanSelection == "Paper" && computerSelection == "Rock"){
+      console.log("You've won this round");
+      humanScore += 1;
+   } else if (humanSelection == "Paper" && computerSelection == "Paper"){
+      console.log("DRAW");
+   } else if (humanSelection == "Paper" && computerSelection == "Scissors"){
+      console.log("Computer won this round");
+      computerScore += 1;
+   } else if (humanSelection == "Scissors" && computerSelection == "Rock"){
+      console.log("Computer won this round");
+      computerScore += 1;
+   } else if (humanSelection == "Scissors" && computerSelection == "Paper"){
+      console.log("You've won this round");
+      humanScore += 1;
+   } else if (humanSelection == "Scissors" && computerSelection == "Scissors"){
+      console.log("DRAW");
+      
+   }
+   console.log("Human: "+ humanScore);
+   console.log("CPU: "+ computerScore);
+   console.log("Human Chose: " + getHumanChoice());
+   console.log("Computer Choose: " + getComputerChoice(3));
 }
 
 
-function capitalize(string){
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-const lower = string.toLowerCase();
-const firstLetter = lower.slice(0,1);
-const firstUpper = firstLetter.toUpperCase();
-const result = firstUpper + lower.slice(1);
-return(result);
-}
+playRound(humanSelection,computerSelection);
 
-//console.log(capitalize(getHumanChoice()));
-   
 
+
+//console.log("Human Chose: " + getHumanChoice());
+//console.log("Computer Choose: " + getComputerChoice(3));
+
+
+/*
 console.log("Computer Choose: " + getComputerChoice(3));
 console.log("Human Chose: " + getHumanChoice());
 
 
 console.log("Human: "+ humanScore);
 console.log("CPU: "+ computerScore);
+*/
