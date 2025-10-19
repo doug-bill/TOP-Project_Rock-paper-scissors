ad36bfd4b19a21cee1c1e426d3e1075afbc16010
doug-bill <65959310+doug-bill@users.noreply.github.com>
2025-10-19 00:24:13 -0300

    Finished the Basic Game functions of Rounds
    ToDo:
    
    -Create a Loop to play 5 Rounds and show the winner and Scores at
    the end.
    
    - Declare the Score inside the playGame function and
    update it from the playRound function.

--- game.js
+++ game.js
@@ -1,3 +1,15 @@
+/*
+create the game Rock Paper Scissors. 
+This game will be played entirely in the console.
+
+1 -  Prompt the player to choose between , Rock, Paper or Scissors and
+save the answer on a variable.
+
+2 - Create a variable that store a random result to go against the players choice
+Math.random[3]
+
+*/
+
 var humanScore = 0;
 var computerScore = 0;
 
@@ -25,58 +37,90 @@ function getHumanChoice(){
     return (result);
 }
 
-function playRound (){
 
-   if (humanSelection == "Rock" && computerSelection == "Rock"){
+function playRound(humanChoice, computerChoice) {
+   
+   if (humanChoice == "Rock" && computerChoice == "Rock"){
       console.log("DRAW");
-     
-   } else if (humanSelection == "Rock" && computerSelection == "Paper"){
-      console.log("Computer won this round");
-      computerScore += 1;
-   } else if (humanSelection == "Rock" && computerSelection == "Scissors"){
-      console.log("You've won this round");
-      humanScore += 1;
-   } else if (humanSelection == "Paper" && computerSelection == "Rock"){
-      console.log("You've won this round");
-      humanScore += 1;
-   } else if (humanSelection == "Paper" && computerSelection == "Paper"){
-      console.log("DRAW");
-   } else if (humanSelection == "Paper" && computerSelection == "Scissors"){
-      console.log("Computer won this round");
-      computerScore += 1;
-   } else if (humanSelection == "Scissors" && computerSelection == "Rock"){
-      console.log("Computer won this round");
-      computerScore += 1;
-   } else if (humanSelection == "Scissors" && computerSelection == "Paper"){
-      console.log("You've won this round");
-      humanScore += 1;
-   } else if (humanSelection == "Scissors" && computerSelection == "Scissors"){
-      console.log("DRAW");
-      
+      console.log("You Choose: "+humanChoice);
+      console.log("CPU Choose: " + computerChoice);
+      } 
+      else if (humanChoice == "Rock" && computerChoice == "Paper"){
+      console.log ("Computer Won!!!");
+      console.log("You Choose: "+humanChoice);
+      console.log("CPU Choose: " + computerChoice);
+      computerScore =+ 1;
+      } 
+      else if (humanChoice == "Rock" && computerChoice == "Scissors"){
+       console.log("You've Won!!!");
+       console.log("You Choose: "+humanChoice);
+       console.log("CPU Choose: " + computerChoice);
+       humanScore =+1;
+      }
+      else if (humanChoice == "Paper" && computerChoice == "Rock"){
+       console.log("You've Won!!!");
+       console.log("You Choose: "+humanChoice);
+       console.log("CPU Choose: " + computerChoice);
+       humanScore =+1;
+      }
+      else if (humanChoice == "Paper" && computerChoice == "Paper"){
+       console.log("DRAW");
+       console.log("You Choose: "+humanChoice);
+       console.log("CPU Choose: " + computerChoice);
+      }
+      else if (humanChoice == "Paper" && computerChoice == "Scissors"){
+       console.log("Computer Won!!!");
+       console.log("You Choose: "+humanChoice);
+       console.log("CPU Choose: " + computerChoice);
+       computerScore =+ 1;
+      }
+      else if (humanChoice == "Scissors" && computerChoice == "Rock"){
+       console.log("Computer Won!!!");
+       console.log("You Choose: "+humanChoice);
+       console.log("CPU Choose: " + computerChoice);
+       computerScore =+ 1;
+      }
+      else if(humanChoice == "Scissors" && computerChoice == "Paper"){
+       console.log("You've Won!!!");
+       console.log("You Choose: "+humanChoice);
+       console.log("CPU Choose: " + computerChoice);
+       humanScore =+1;
+      }
+      else {
+       console.log("DRAW");
+       console.log("You Choose: "+humanChoice);
+       console.log("CPU Choose: " + computerChoice);
    }
-   console.log("Human: "+ humanScore);
-   console.log("CPU: "+ computerScore);
-   console.log("Human Chose: " + getHumanChoice());
-   console.log("Computer Choose: " + getComputerChoice(3));
 }
 
+//const humanSelection = getHumanChoice();
+//const computerSelection = getComputerChoice(3);
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+//playRound(humanSelection, computerSelection);
 
-playRound(humanSelection,computerSelection);
-
-
-
-//console.log("Human Chose: " + getHumanChoice());
-//console.log("Computer Choose: " + getComputerChoice(3));
-
+//console.log("Player Score: " + humanScore);
+//console.log("CPU Score: "+ computerScore);
 
 /*
-console.log("Computer Choose: " + getComputerChoice(3));
-console.log("Human Chose: " + getHumanChoice());
 
-
-console.log("Human: "+ humanScore);
-console.log("CPU: "+ computerScore);
+    Create a new function named playGame.
+    Move your playRound function and score variables so that they’re declared inside of the new playGame function
+    Play 5 rounds by calling playRound 5 times.
+        Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to recall the choice functions to get new choices for each round.
+        Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the return values to something more useful.
+        If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
 */
+
+function playGame(){
+
+   const humanSelection = getHumanChoice();  
+   const computerSelection = getComputerChoice(3);
+
+   playRound(humanSelection, computerSelection);
+   
+   console.log("Player Score: " + humanScore);
+   console.log("CPU Score: "+ computerScore);
+
+}
+
+playGame();
