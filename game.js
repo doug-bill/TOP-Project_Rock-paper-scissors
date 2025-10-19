851a75e408ff1198cefd0080ff0c8d3f2cc00862
doug-bill <65959310+doug-bill@users.noreply.github.com>
2025-10-19 09:59:56 -0300

    Just deleted the comments that discribed the ToDo.

--- game.js
+++ game.js
@@ -1,15 +1,3 @@
-/*
-create the game Rock Paper Scissors. 
-This game will be played entirely in the console.
-
-1 -  Prompt the player to choose between , Rock, Paper or Scissors and
-save the answer on a variable.
-
-2 - Create a variable that store a random result to go against the players choice
-Math.random[3]
-
-*/
-
 var humanScore = 0;
 var computerScore = 0;
 
@@ -93,15 +81,6 @@ function playRound(humanChoice, computerChoice) {
    }
 }
 
-/*
-
-    Create a new function named playGame.
-    Move your playRound function and score variables so that they’re declared inside of the new playGame function
-    Play 5 rounds by calling playRound 5 times.
-        Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to recall the choice functions to get new choices for each round.
-        Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the return values to something more useful.
-        If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
-*/
 
 function playGame(){
 
